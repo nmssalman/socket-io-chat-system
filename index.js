292977d03b1b85ec4061b6d790739ec05e0b195a
nmssalman <nmssalman@outlook.com>
2025-04-19 00:20:20 +0530

    fix

--- index.js
+++ index.js
@@ -50,7 +50,9 @@ io.on('connection', (socket) => {
   });
 });
 
+const PORT = process.env.PORT || 3000;  // Heroku uses process.env.PORT
+
 // Start the server on port 3000
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+server.listen(PORT, () => {
+  console.log('Server is running on %s', PORT);
 });
