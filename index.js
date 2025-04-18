4b356f3cccfeea6201d2bc37dad31b16a6981b27
nmssalman <nmssalman@outlook.com>
2025-04-18 23:16:58 +0530

    fixes

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log('✅ Server running on http://localhost:3000');
+  console.log('✅ Server running on port:3000');
 });
 
 //
